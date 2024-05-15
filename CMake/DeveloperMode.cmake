f58187895ce3930b952173404f501d3c346223c0
Patrick Kappl <patrick.kappl@fantana.at>
2024-05-15 15:57:27 +0200

    Remove custom target run-exe

--- CMake/DeveloperMode.cmake
+++ CMake/DeveloperMode.cmake
@@ -5,13 +5,6 @@ if(BUILD_TESTING)
   add_subdirectory(Tests)
 endif()
 
-add_custom_target(
-    run-exe
-    COMMAND BullseyeFireControlSystem_Exe
-    VERBATIM
-)
-add_dependencies(run-exe BullseyeFireControlSystem_Exe)
-
 option(ENABLE_COVERAGE "Enable coverage support separate from CTest's" OFF)
 if(ENABLE_COVERAGE)
   include(CMake/Coverage.cmake)
