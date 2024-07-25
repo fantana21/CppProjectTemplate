c383534b9d28749ae60912641fb5a8b53a7345b9
Patrick Kappl <patrick.kappl@fantana.at>
2024-07-25 08:49:43 +0200

    Use gcovr instead of LCOV for coverage reports

--- CMake/Coverage.cmake
+++ CMake/Coverage.cmake
@@ -1,26 +1,37 @@
-# TODO: Exclude source files in Tests/
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(GCOV_EXECUTABLE "llvm-cov gcov")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    set(GCOV_EXECUTABLE "gcov")
+else()
+    message(
+        FATAL_ERROR "Unsupported compiler for generating coverage info: ${CMAKE_CXX_COMPILER_ID}"
+    )
+endif()
 
-# ---- Variables ----
-
-# We use variables separate from what CTest uses, because those have customization issues
-set(COVERAGE_TRACE_COMMAND
-    lcov -c -q -o "${PROJECT_BINARY_DIR}/coverage.info" -d "${PROJECT_BINARY_DIR}" --include
-    "${PROJECT_SOURCE_DIR}/CppProjectTemplate*"
-    CACHE STRING "; separated command to generate a trace for the 'coverage' target"
+set(GENERATE_COVERAGE_REPORTS
+    gcovr
+    --gcov-executable
+    "${GCOV_EXECUTABLE}"
+    --root
+    "${PROJECT_SOURCE_DIR}"
+    --txt
+    "${PROJECT_BINARY_DIR}/Coverage.txt"
+    --html
+    --html-details
+    "${PROJECT_BINARY_DIR}/CoverageHtml/"
+    --html-theme
+    github.dark-green
+    --cobertura
+    "${PROJECT_BINARY_DIR}/Coverage.xml"
+    --cobertura-pretty
+    CACHE STRING
+          "; separated command to generate a text, HTML, and XML report for the 'coverage' target"
 )
-
-set(COVERAGE_HTML_COMMAND
-    genhtml --legend -f -q "${PROJECT_BINARY_DIR}/coverage.info" -p "${PROJECT_SOURCE_DIR}" -o
-    "${PROJECT_BINARY_DIR}/coverage_html"
-    CACHE STRING "; separated command to generate an HTML report for the 'coverage' target"
-)
-
-# ---- Coverage target ----
 
 add_custom_target(
     coverage
-    COMMAND ${COVERAGE_TRACE_COMMAND}
-    COMMAND ${COVERAGE_HTML_COMMAND}
-    COMMENT "Generating coverage report"
+    COMMAND ${GENERATE_COVERAGE_REPORTS}
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    COMMENT "Generating plain text, HTML, and XML coverage reports"
     VERBATIM
 )
